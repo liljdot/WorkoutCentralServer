59e9bd0b3c8df75451e4e945f3ed4919225885e2
liljdot <jehuoki@gmail.com>
2025-01-28 22:00:17 +0100

    edit cors header

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const PORT = process.env.PORT
     app.use(cors())
 
     app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, HEAD, DELETE");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type");
         next();
       })
