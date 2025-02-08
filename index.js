6c67e3ea58a42d370edb1675b9726ecfa152a3b2
liljdot <jehuoki@gmail.com>
2025-02-08 21:12:06 +0100

    allow localhost

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ const PORT = process.env.PORT
 
     // allow cors. the options allow cookies from/to all origins
     app.use(cors({
-        origin: "*",
+        origin: "http://localhost:5173",
         credentials: true
     }))
 
     app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
         res.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, HEAD, DELETE");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type");
         next();
